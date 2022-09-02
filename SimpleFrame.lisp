66ce91082c358e679ff248ac854c78e43ead978d
danielhaden <daniel.haden@protonmail.com>
2022-09-01 22:32:51 -0600

    added comments

--- SimpleFrame.lisp
+++ SimpleFrame.lisp
@@ -4,16 +4,25 @@
 
 (in-package #:clim-simple-frame-app)
 
+;; This macro defines the outermost application frame
 (define-application-frame simple-frame-app ()
+
+  ;; A list of superclasses that the application frame can
+  ;; inherit from
   ()
+
+  ;; Defines the panes that will be defined in the frame. This
+  ;; specifies a single pane which is an interactor
   (:panes
-   (int :interactor
-	:height 400
-	:width 600))
+   (my-interactor :interactor
+                  :height 400
+		  :width 600))
+  
   ;; defines how a pane is positioned inside
   ;; the application frame.
   (:layouts
-   (default int)))
+   (default my-interactor)))
 
+;; This function is called to run the application
 (defun run-app ()
   (run-frame-top-level (make-application-frame 'simple-frame-app)))
